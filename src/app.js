a57ac7508ccb5c8e352b119efb1db925eb05bbcd
Aryan <aryan602.as@gmail.com>
2023-10-30 23:45:25 +0530

    added json file for cbc reference data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-
-
-
 const app =express();
-app.use(express.urlencoded({ extended: true }));
+
+
+
+app.use(express.json());
 // Import route files
 const CBCRouter = require('./routes/CBCroute');
 
--- src/routes/CBCroute.js
+++ src/routes/CBCroute.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../public/cbcData.json');
 
+router.post('/', (req, res) => {
+  console.log(data["White Blood Cell (WBC)"]);
 
-// Define routes for user management
-router.get('/', (req, res) => {
-  res.send('User list page.');
+  const requestBody = req.body;
+
+  if (!requestBody || ! requestBody.report) {
+    // Handle invalid or missing request body
+    res.status(400).send('Invalid request body');
+    return;
+  }
+  let resdata = {};
+
+  for (const key in requestBody.report) {
+    if (requestBody.report.hasOwnProperty(key)) {
+      const parameterValue = requestBody.report[key];
+      resdata[key] = parameterValue; // Assign the parameter value to the corresponding key in resdata
+    }
+  }
+
+  res.send(resdata);
 });
-
-
-
 
 module.exports = router;

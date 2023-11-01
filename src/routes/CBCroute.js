e52cb7802037d1ad1733fe4494e082b26fa1e363
Aryan <aryan602.as@gmail.com>
2023-11-02 02:25:55 +0530

    refined json data and setup api endpoints

--- src/routes/CBCroute.js
+++ src/routes/CBCroute.js
@@ -3,21 +3,40 @@ const router = express.Router();
 const data = require('../public/cbcData.json');
 
 router.post('/', (req, res) => {
-  console.log(data["White Blood Cell (WBC)"]);
-
   const requestBody = req.body;
 
-  if (!requestBody || ! requestBody.report) {
-    // Handle invalid or missing request body
-    res.status(400).send('Invalid request body');
-    return;
+  if (!requestBody || !requestBody.report) {
+    return res.status(400).send('Invalid request body');
   }
-  let resdata = {};
+
+  const { sex, age } = requestBody.info;
+  const age_group = age < 18 ? '0-17' : age < 50 ? '18-49' : '50+';
+
+  const resdata = {};
 
   for (const key in requestBody.report) {
-    if (requestBody.report.hasOwnProperty(key)) {
-      const parameterValue = requestBody.report[key];
-      resdata[key] = parameterValue; // Assign the parameter value to the corresponding key in resdata
+    if (requestBody.report.hasOwnProperty(key) && data[key] && data[key]["What It Is"]) {
+      const reference = data[key]["Reference Values"][sex][age_group];
+      const paramvalue = requestBody.report[key];
+      const paramobj = {
+        description: data[key]["What It Is"],
+      };
+
+      if (paramvalue < reference["Lower Limit"]) {
+        paramobj.result = "below limit";
+        paramobj.effects = data[key]["Effects"]["Below Limit"];
+        paramobj.causes = data[key]["Possible Causes"]["Below Limit"];
+        paramobj.fixes = data[key]["Natural Fixes"];
+      } else if (paramvalue <= reference["Upper Limit"]) {
+        paramobj.result = "ok";
+      } else {
+        paramobj.result = "above limit";
+        paramobj.effects = data[key]["Effects"]["Above Limit"];
+        paramobj.causes = data[key]["Possible Causes"]["Above Limit"];
+        paramobj.fixes = data[key]["Natural Fixes"];
+      }
+
+      resdata[key] = paramobj;
     }
   }
 

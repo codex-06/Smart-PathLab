ee60dbb62853d2bda15b45d7b332c92f5b28d907
Aryan <aryan602.as@gmail.com>
2023-11-04 00:38:32 +0530

    minor change

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ const app =express();
 
 
 
+
+app.use(cors());
 app.use(express.json());
 // Import route files
 const CBCRouter = require('./routes/CBCroute');
-
 // Use the imported route files as middleware
 app.use('/cbc', CBCRouter);
 
 
-app.use(cors());
 
 // Start the Express server
 const port = process.env.PORT || 3000;
